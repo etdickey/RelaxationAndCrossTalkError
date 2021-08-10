783fd09f2ce998aac1c345fe5d553eb4fa0604f2
Ethan Dickey <Ethan.Dickey@ibm.com>
2021-08-10 17:45:57 -0600

    Added delay relaxation pass py file.

---
+++ delayPasses/delayRelaxationPass.py
@@ -0,0 +1,41 @@
+#Delay replace pass
+    class DelayReplace(TransformationPass):
+        """Adds delay error information to circuit metadata for noise model post processing"""
+
+        def getNewLabel(self, duration):
+            """Generate a unique label based on the duration of the delay
+
+            Args:
+                duration (float): the duration of the delay op
+
+            Returns:
+                String: the unique label based on duration
+            """
+            return f'id_delay_{duration}'
+
+        def run(self, dag):
+            """Run the DelayReplace pass on 'dag'.
+
+            Args:
+                dag (DAGCircuit): the DAG to be optimized
+
+            Returns:
+                DAGCircuit: the optimized DAG.
+            """
+
+            if not dag.metadata:
+                dag.metadata = {}
+
+            if "transpiler_parameterized_error" not in dag.metadata:
+                dag.metadata['transpiler_parameterized_error'] = {}
+
+            #iterate over delay nodes
+            for node in dag.named_nodes("delay"):
+                node_label = self.getNewLabel(node._op.duration)
+                node._op.label = node_label
+
+
+                #save error in metadata
+                dag.metadata['transpiler_parameterized_error'][(node_label, tuple(node.qargs))] = [node._op.duration]
+
+            return dag
